6e9b039a7610831f92e5e8cc63ec037ff803ac38
prpost <prpost@ttu.ee>
2023-05-07 16:46:07 +0300

    Add forms

---
+++ frmMSAccessConnect.designer.cs
@@ -0,0 +1,107 @@
+﻿namespace NppDB.MSAccess
+{
+    partial class frmMSAccessConnect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.rdoOpen = new System.Windows.Forms.RadioButton();
+            this.rdoNew = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(120, 113);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(64, 36);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Cancel";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(43, 113);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(64, 36);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // rdoOpen
+            // 
+            this.rdoOpen.AutoSize = true;
+            this.rdoOpen.Checked = true;
+            this.rdoOpen.Location = new System.Drawing.Point(43, 50);
+            this.rdoOpen.Name = "rdoOpen";
+            this.rdoOpen.Size = new System.Drawing.Size(136, 17);
+            this.rdoOpen.TabIndex = 1;
+            this.rdoOpen.TabStop = true;
+            this.rdoOpen.Text = "Open an existing database";
+            this.rdoOpen.UseVisualStyleBackColor = true;
+            // 
+            // rdoNew
+            // 
+            this.rdoNew.AutoSize = true;
+            this.rdoNew.Location = new System.Drawing.Point(43, 27);
+            this.rdoNew.Name = "rdoNew";
+            this.rdoNew.Size = new System.Drawing.Size(137, 17);
+            this.rdoNew.TabIndex = 0;
+            this.rdoNew.Text = "Create an empty database ";
+            this.rdoNew.UseVisualStyleBackColor = true;
+            // 
+            // frmMSAccessConnect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 191);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.rdoOpen);
+            this.Controls.Add(this.rdoNew);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMSAccessConnect";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MSAccess Connect";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.RadioButton rdoOpen;
+        private System.Windows.Forms.RadioButton rdoNew;
+    }
+}
---
+++ frmPassword.designer.cs
@@ -0,0 +1,106 @@
+﻿namespace NppDB.MSAccess
+{
+    partial class frmPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.cbxShowPwd = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(140, 104);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 33);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Cancel";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(50, 104);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 33);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(50, 36);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(164, 21);
+            this.txtPwd.TabIndex = 9;
+            // 
+            // cbxShowPwd
+            // 
+            this.cbxShowPwd.AutoSize = true;
+            this.cbxShowPwd.Location = new System.Drawing.Point(74, 72);
+            this.cbxShowPwd.Name = "cbxShowPwd";
+            this.cbxShowPwd.Size = new System.Drawing.Size(115, 16);
+            this.cbxShowPwd.TabIndex = 10;
+            this.cbxShowPwd.Text = "show password";
+            this.cbxShowPwd.UseVisualStyleBackColor = true;
+            this.cbxShowPwd.CheckedChanged += new System.EventHandler(this.cbxShowPwd_CheckedChanged);
+            // 
+            // frmPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 176);
+            this.ControlBox = false;
+            this.Controls.Add(this.cbxShowPwd);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPassword";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Password";
+            this.Load += new System.EventHandler(this.frmPassword_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.CheckBox cbxShowPwd;
+    }
+}
